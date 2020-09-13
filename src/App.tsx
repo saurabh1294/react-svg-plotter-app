37d11eef95b31ee71a137a42e7f9fa9fbd584706
saurabh1294 <saurabh.gattani@gmail.com>
2020-09-13 22:15:52 +1000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import SvgPlotter from './components/SvgPlotter/SvgPlotter';
+import './App.css';
+
+export default class App extends React.Component {
+  state = {
+    showSvg: false
+  };
+
+  points: string[] = [];
+
+  svgCommand = `Enter SVG plotter command in the below format:- 
+  R <X Coordinate> <Y Coordinate> <Width> <Height> - Should Draw a rectangle with the parameters marked onto the SVG
+  C <CX Coordinate> <CY Coordinate> <Radius> - Should Draw a circle with the parameters marked onto the SVG
+  P <X1,Y1> <X2,Y2> <X3,Y3> ..... <Xn,Yn> - Should draw a polygon onto the SVG with the points specified`;
+
+  render() {
+      return (
+        <div className="App">
+          <textarea onChange={this.captureCommand.bind(this)} 
+          placeholder={this.svgCommand} rows={10} cols={100} />
+          <div>
+            <button onClick={this.drawSvg.bind(this)}>Click me</button>
+          </div>
+          {this.state.showSvg && <SvgPlotter command={this.svgCommand}/>}
+        </div>
+      );
+    }
+
+    captureCommand(event: any) {
+      this.svgCommand = (event?.target?.value);
+      this.setState({showSvg: false});
+    }
+
+
+    drawSvg() {
+      this.setState({showSvg: true});
+    }
+}
+
---
+++ src/components/SvgPlotter/SvgPlotter.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+
+export default class SvgPlotter extends React.Component <any> {
+    componentDidMount() {
+        console.log(this.props);
+        const {command} = this.props;
+        this.parseAndRunSvgPlotterCommand(command.split("\n"));
+    }
+
+    render() {
+      return (
+        <div>
+            <svg id="polygon" width="250" height="250">
+            </svg>
+            <svg id="circle" width="250" height="250">
+            </svg>
+            <svg id="rectangle" width="250" height="250">
+            </svg>
+        </div>
+      );
+    }
+
+    plotSvg(points: any) {
+        var svgns = "http://www.w3.org/2000/svg";
+            let svg = document.getElementById('circle') as any;
+            var shape = document.createElementNS(svgns, "circle");
+            shape.setAttributeNS(null, "cx", points[1]);
+            shape.setAttributeNS(null, "cy", points[2]);
+            shape.setAttributeNS(null, "r",  points[3]);
+            shape.setAttributeNS(null, "fill", "green");
+            svg.appendChild(shape);
+        
+    }
+
+    parseAndRunSvgPlotterCommand(lines: any) {
+        let points = [];
+        let svgns = "http://www.w3.org/2000/svg";
+        let svg;
+
+        for (let i = 0; i < lines.length; i++) {
+          let line = lines[i];
+          console.log('line', line)
+          points = line.split(' ');
+  
+          switch(line[0]?.toUpperCase()) {
+            case 'C':
+                console.log('drawing circle', points, this.props);
+                svg = document.getElementById('circle') as any;
+                var shape = document.createElementNS(svgns, "circle");
+                shape.setAttributeNS(null, "cx", points[1]);
+                shape.setAttributeNS(null, "cy", points[2]);
+                shape.setAttributeNS(null, "r",  points[3]);
+                shape.setAttributeNS(null, 'fill', '#'+Math.round(0xffffff * Math.random()).toString(16));
+                svg.appendChild(shape);
+              break;
+  
+            case 'P':
+                console.log('drawing Polygon', points, this.props);
+                svg = document.getElementById('polygon') as any;
+                let ppoints = points.splice(1).join(" ").split(",").join(",");
+                let polygon = document.createElementNS(svgns, 'polygon');
+                polygon.setAttributeNS(null, 'points', ppoints.trim());
+                polygon.setAttributeNS(null, 'fill', '#'+Math.round(0xffffff * Math.random()).toString(16));
+                svg.appendChild(polygon);
+              break;
+  
+            case 'R':
+                console.log('drawing Rectangle', points, this.props);
+                svg = document.getElementById('rectangle') as any;
+                let rect = document.createElementNS(svgns, 'rect');
+                rect.setAttributeNS(null, 'x', points[1]);
+                rect.setAttributeNS(null, 'y', points[2]);
+                rect.setAttributeNS(null, 'height', points[3]);
+                rect.setAttributeNS(null, 'width', points[4]);
+                rect.setAttributeNS(null, 'fill', '#'+Math.round(0xffffff * Math.random()).toString(16));
+                svg.appendChild(rect);
+              break;
+  
+            default:
+                console.log('Sorry we don\'t recognize the shape/command');
+          }
+        }
+      }
+}
+
